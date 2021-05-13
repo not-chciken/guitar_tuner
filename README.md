1ba0146479ca423c2ebdeaf91c02a3dad6155b2f
Niko Zurstraßen <niko.zurstrassen@rwth-aachen.de>
2021-05-13 22:28:33 +0200

    added linting; fixing a note buffer bug

---
+++ README.md
@@ -0,0 +1,25 @@
+# Python Guitar Tuner
+In this repository you can find a simple __python guitar tuner__ based on the _Harmonic Product Spectrum_.
+Just execute the `hps_tuner_plt.py` script and enjoy a tuned guitar. \
+A thorough explanation of the underlying theoretical concepts can be found on my [website](https://www.chciken.com/digital/signal/processing/2020/05/13/guitar-tuner.html). \
+If you want to fine-tune the tuner (hehe), just fiddle with the following script parameters:
+``` python
+# The sample frequency in Hz. With 48kHz sampling frequency we can capture
+# all frequencies from 0Hz to 24kHz which is more than sufficient for most
+# instruments.
+SAMPLE_FREQ = 48000
+# Window size of the DFT in samples.
+# A good compromise between frequency resolution and delay is a window
+# size of 48000. This corresponds to a 1 second delay and an frequency
+# resolution of 1 Hz assuming a sampling frequency of 48kHz
+WINDOW_SIZE = 48000
+# Number of samples the DFT window step size
+# With 12000 samples every 0.25s a new tuning process is executed.
+WINDOW_STEP = 12000
+# Maximum number of harmonic product spectrums
+NUM_HPS = 8
+# Tuning is activated if the signal power exceeds this threshold
+POWER_THRESH = 7e-6
+# Deinition of the concert pitch 'a1'
+CONCERT_PITCH = 440
+```
--- hps_tuner.py
+++ hps_tuner.py
@@ -1,106 +1,127 @@
-import sounddevice as sd
+'''
+Guitar tuner script based on the Harmonic Product Spectrum (HPS)
+
+MIT License
+Copyright (c) 2021 chciken
+'''
+
+import copy
+import os
 import numpy as np
 import scipy.fftpack
-import os
-import matplotlib.pyplot as plt
-import copy
+import sounddevice as sd
+import time
 
-# General settings
+# General settings that can be changed by the user
 SAMPLE_FREQ = 48000 # sample frequency in Hz
 WINDOW_SIZE = 48000 # window size of the DFT in samples
 WINDOW_STEP = 12000 # step size of window
+NUM_HPS = 8 # max number of harmonic product spectrums
+POWER_THRESH = 1e-6 # tuning is activated if the signal power exceeds this threshold
+CONCERT_PITCH = 440 # defining a1
+
 WINDOW_T_LEN = WINDOW_SIZE / SAMPLE_FREQ # length of the window in seconds
 SAMPLE_T_LENGTH = 1 / SAMPLE_FREQ # length between two samples in seconds
-NUM_HPS = 8 #max number of harmonic product spectrums
-DELTA_FREQ = (SAMPLE_FREQ/WINDOW_SIZE) # frequency step width of the interpolated DFT
-windowSamples = [0 for _ in range(WINDOW_SIZE)]
-noteBuffer = ["1","2","3"]
+DELTA_FREQ = SAMPLE_FREQ / WINDOW_SIZE # frequency step width of the interpolated DFT
+OCTAVE_BANDS = [50, 100, 200, 400, 800, 1600, 3200, 6400, 12800, 25600]
 
-# This function finds the closest note for a given pitch
-# Returns: note (e.g. a, g#, ..), pitch of the tone
-CONCERT_PITCH = 440
 ALL_NOTES = ["A","A#","B","C","C#","D","D#","E","F","F#","G","G#"]
 def find_closest_note(pitch):
-  i = int( np.round( np.log2( pitch/CONCERT_PITCH )*12 ) )
-  clostestNote = ALL_NOTES[i%12] + str(4 + np.sign(i) * int( (9+abs(i))/12 ) )
-  closestPitch = CONCERT_PITCH*2**(i/12)
-  return clostestNote, closestPitch
+  """
+  This function finds the closest note for a given pitch
+  Parameters:
+    pitch (float): pitch given in hertz
+  Returns:
+    closest_note (str): e.g. a, g#, ..
+    closest_pitch (float): pitch of the closest note in hertz
+  """
+  i = int(np.round(np.log2(pitch/CONCERT_PITCH)*12))
+  closest_note = ALL_NOTES[i%12] + str(4 + np.sign(i) * int((9+abs(i))/12))
+  closest_pitch = CONCERT_PITCH*2**(i/12)
+  return closest_note, closest_pitch
 
-hannWindow = np.hanning(WINDOW_SIZE)
+HANN_WINDOW = np.hanning(WINDOW_SIZE)
 def callback(indata, frames, time, status):
-  global windowSamples, lastNote
+  """
+  Callback function of the InputStream method.
+  That's where the magic happens ;)
+  """
+  # define static variables
+  if not hasattr(callback, "window_samples"):
+    callback.window_samples = [0 for _ in range(WINDOW_SIZE)]
+  if not hasattr(callback, "noteBuffer"):
+    callback.noteBuffer = ["1","2"]
+
   if status:
     print(status)
+    return
   if any(indata):
-    windowSamples = np.concatenate((windowSamples,indata[:, 0])) # append new samples
-    windowSamples = windowSamples[len(indata[:, 0]):] # remove old samples
+    callback.window_samples = np.concatenate((callback.window_samples, indata[:, 0])) # append new samples
+    callback.window_samples = callback.window_samples[len(indata[:, 0]):] # remove old samples
 
-    signalPower = (np.linalg.norm(windowSamples, ord=2)**2) / len(windowSamples)
-    if signalPower < 5e-7:
+    # skip if signal power is too low
+    signal_power = (np.linalg.norm(callback.window_samples, ord=2)**2) / len(callback.window_samples)
+    if signal_power < POWER_THRESH:
       os.system('cls' if os.name=='nt' else 'clear')
       print("Closest note: ...")
       return
 
-    hannSamples = windowSamples * hannWindow
-    magnitudeSpec = abs( scipy.fftpack.fft(hannSamples)[:len(hannSamples)//2] )
+    # avoid spectral leakage by multiplying the signal with a hann window
+    hann_samples = callback.window_samples * HANN_WINDOW
+    magnitude_spec = abs(scipy.fftpack.fft(hann_samples)[:len(hann_samples)//2])
 
-    #supress mains hum
+    # supress mains hum, set everything below 62Hz to zero
     for i in range(int(62/DELTA_FREQ)):
-      magnitudeSpec[i] = 0
+      magnitude_spec[i] = 0
 
-    #Calculate average energy per frequency for the octave bands
-    octaveBands = [50,100,200,400,800,1600,3200,6400,12800,25600]
-    for j in range(len(octaveBands)-1):
-      indStart = int(octaveBands[j]/DELTA_FREQ)
-      indEnd = int(octaveBands[j+1]/DELTA_FREQ)
-      indEnd = indEnd if len(magnitudeSpec) > indEnd else len(magnitudeSpec)
-      avgEnergPerFreq = 1*(np.linalg.norm(magnitudeSpec[indStart:indEnd], ord=2)**2) / (indEnd-indStart)
-      avgEnergPerFreq = avgEnergPerFreq**0.5
-      for i in range(indStart, indEnd):
-        magnitudeSpec[i] = magnitudeSpec[i] if magnitudeSpec[i] > avgEnergPerFreq else 0  #suppress white noise
+    # calculate average energy per frequency for the octave bands
+    # and suppress everything below it
+    for j in range(len(OCTAVE_BANDS)-1):
+      ind_start = int(OCTAVE_BANDS[j]/DELTA_FREQ)
+      ind_end = int(OCTAVE_BANDS[j+1]/DELTA_FREQ)
+      ind_end = ind_end if len(magnitude_spec) > ind_end else len(magnitude_spec)
+      avg_energy_per_freq = (np.linalg.norm(magnitude_spec[ind_start:ind_end], ord=2)**2) / (ind_end-ind_start)
+      avg_energy_per_freq = avg_energy_per_freq**0.5
+      for i in range(ind_start, ind_end):
+        magnitude_spec[i] = magnitude_spec[i] if magnitude_spec[i] > avg_energy_per_freq else 0
 
-    #Interpolate spectrum
-    magSpecIpol = np.interp(np.arange(0, len(magnitudeSpec), 1/NUM_HPS), np.arange(0, len(magnitudeSpec)), magnitudeSpec)
-    magSpecIpol = magSpecIpol / np.linalg.norm(magSpecIpol, ord=2) #normalize it
+    # interpolate spectrum
+    mag_spec_ipol = np.interp(np.arange(0, len(magnitude_spec), 1/NUM_HPS), np.arange(0, len(magnitude_spec)),
+                              magnitude_spec)
+    mag_spec_ipol = mag_spec_ipol / np.linalg.norm(mag_spec_ipol, ord=2) #normalize it
 
-    hpsSpec = copy.deepcopy(magSpecIpol)
+    hps_spec = copy.deepcopy(mag_spec_ipol)
 
+    # calculate the HPS
     for i in range(NUM_HPS):
-      tmpHpsSpec = np.multiply(hpsSpec[:int(np.ceil(len(magSpecIpol)/(i+1)))], magSpecIpol[::(i+1)])
-      if not any(tmpHpsSpec):
+      tmp_hps_spec = np.multiply(hps_spec[:int(np.ceil(len(mag_spec_ipol)/(i+1)))], mag_spec_ipol[::(i+1)])
+      if not any(tmp_hps_spec):
         break
-      hpsSpec = tmpHpsSpec
+      hps_spec = tmp_hps_spec
 
-    maxInd = np.argmax(hpsSpec)
-    maxFreq = maxInd * (SAMPLE_FREQ/WINDOW_SIZE) / NUM_HPS
+    max_ind = np.argmax(hps_spec)
+    max_freq = max_ind * (SAMPLE_FREQ/WINDOW_SIZE) / NUM_HPS
 
-    closestNote, closestPitch = find_closest_note(maxFreq)
-    maxFreq = round(maxFreq, 1)
-    closestPitch = round(closestPitch, 1)
+    closest_note, closest_pitch = find_closest_note(max_freq)
+    max_freq = round(max_freq, 1)
+    closest_pitch = round(closest_pitch, 1)
 
-    noteBuffer.insert(0,closestNote) #note that this is a ringbuffer
-    noteBuffer.pop()
+    callback.noteBuffer.insert(0, closest_note) # note that this is a ringbuffer
+    callback.noteBuffer.pop()
 
-    majorityVote = max(set(noteBuffer), key = noteBuffer.count)
-
-    if noteBuffer.count(majorityVote) > 1:
-      detectedNote = majorityVote
-    else:
-      return
     os.system('cls' if os.name=='nt' else 'clear')
-    print(f"Closest note: {closestNote} {maxFreq}/{closestPitch}")
+    if callback.noteBuffer.count(callback.noteBuffer[0]) == len(callback.noteBuffer):
+      print(f"Closest note: {closest_note} {max_freq}/{closest_pitch}")
+    else:
+      print(f"Closest note: ...")
 
   else:
     print('no input')
 
 try:
   print("Starting HPS guitar tuner...")
-  with sd.InputStream(channels=1, callback=callback,
-    blocksize = WINDOW_STEP,
-    samplerate = SAMPLE_FREQ):
+  with sd.InputStream(channels=1, callback=callback, blocksize=WINDOW_STEP, samplerate=SAMPLE_FREQ):
     while True:
-      response = input()
-      if response in ('', 'q', 'Q'):
-        break
-except Exception as e:
-    print(str(e))
+      time.sleep(0.5)
+except Exception as exc:
+  print(str(exc))
